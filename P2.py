e792f3a4fe18fcfa3c118af23e68c1d97c140e0f
jnealon1 <jnealon1@student.ccc.edu>
2024-09-24 18:50:16 -0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,14 @@
+width=float(input ('enter a width'))
+height =float(input ('enter a height'))
+total= width * height
+print('Area is', total)
+
+
+base=float(input('enter a base'))
+height=float(input('enter a hegith'))
+total=0.5 * base * height
+print('Area is' , total)
+
+radius=float(input('enter radius'))
+total= 3.1459 * radius**2
+print('Area is' , total)
